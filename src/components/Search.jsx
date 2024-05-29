5989bffdf108993700755fbcc838ab41ec74ce37
habib-technonext <habibur.rahman@technonext.com>
2024-05-29 17:08:55 +0600

    show api response and hide api response data using useffect

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,6 +6,7 @@ const URL = "https://jsonplaceholder.typicode.com/posts";
 const Search = () => {
   const [query, setQuery] = useState("");
   const [data, setData] = useState([]);
+  const [showData, setShowData] = useState(false);
 
   useEffect(() => {
     async function fetchFood() {
@@ -15,22 +16,36 @@ const Search = () => {
           setData(data);
         });
     }
-    fetchFood();
-  }, [query]);
+    if (showData) {
+      fetchFood();
+    }
+  }, [query, showData]);
   return (
-    <div>
+    <div className="container mx-auto px-4">
       <input
         onChange={(e) => setQuery(e.target.value)}
         type="text"
         value={query}
+        className="border border-green-300 rounded-md py-2 px-4 mb-4"
+        placeholder="Search here"
       />
       <br />
       <br />
-      <div>
-        {data.map((item, id) => (
-          <li key={id}>{item.title}</li>
-        ))}
-      </div>
+      <button onClick={() => setShowData(!showData)} className="mb-4">
+        {showData ? "Hide Data" : "Show Data"}
+      </button>
+      {showData && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {data?.map((item, id) => (
+            <div key={id} className="bg-green-400 rounded-md shadow-md p-4">
+              <h2 className="text-lg text-black font-semibold mb-2">
+                {item?.title}
+              </h2>
+              <p className="text-gray-700">{item?.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
